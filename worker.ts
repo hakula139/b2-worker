68ae138ac474e824fb6455b8abc7d5474ddd76f3
Hakula Chen <i@hakula.xyz>
2025-07-30 01:57:23 +0800

    refactor: refactor with typescript, initialize a project

--- worker.ts
+++ worker.ts
@@ -1,5 +1,11 @@
+import { ExecutionContext, ExportedHandler } from '@cloudflare/workers-types';
+
+export interface Env {
+  B2_HOSTNAME: string;
+}
+
 export default {
-  async fetch(request, env) {
+  async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url);
     const originalPath = url.pathname;
     const newPath = `/file${originalPath}`;
@@ -8,18 +14,15 @@ export default {
     newRequest.headers.set('Host', env.B2_HOSTNAME);
 
     const response = await fetch(newRequest);
-
     if (!response.ok) {
       return response;
     }
 
     const newResponse = new Response(response.body, response);
-    const filename = originalPath.split('/').pop();
-
-    // Divide the filename into two parts: UUID and original filename
-    const filenameParts = filename.split('_', 2);
-    if (filenameParts.length > 1) {
-      const originalFilename = filenameParts[1];
+    // Split the filename into two parts: UUID and original filename
+    const originalFilename = originalPath.split('/').pop()?.split('_', 2).at(1);
+    if (originalFilename) {
+      // Set the Content-Disposition header to force the browser to download the file with the original filename
       newResponse.headers.set(
         'Content-Disposition',
         `attachment; filename*=UTF-8''${encodeURIComponent(originalFilename)}`,
@@ -28,4 +31,4 @@ export default {
 
     return newResponse;
   },
-};
+} satisfies ExportedHandler<Env>;
