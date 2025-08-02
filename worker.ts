0d24a39d84482c08b59f45fedc1d8825e7dba386
Hakula Chen <i@hakula.xyz>
2025-08-03 03:51:39 +0800

    fix: fix 401 unauthorized issue

--- worker.ts
+++ worker.ts
@@ -7,14 +7,7 @@ export interface Env {
 export default {
   async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url);
-    const originalPath = url.pathname;
-    const newPath = `/file${originalPath}`;
-    const newUrl = new URL(newPath, `https://${env.B2_HOSTNAME}`);
-
-    // Forward all query parameters (including signature parameters for private buckets)
-    for (const [key, value] of url.searchParams) {
-      newUrl.searchParams.set(key, value);
-    }
+    const newUrl = `https://${env.B2_HOSTNAME}${url.pathname}${url.search}`;
 
     const newRequest = new Request(newUrl, request);
     newRequest.headers.set('Host', env.B2_HOSTNAME);
